80dd9a9ea38749b4e1f3a8e5bc32b5792e115e59
Nikita Grishin <ng222jk@student.lnu.se>
2024-01-20 10:23:00 +0100

    refactor some

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/LizaSil
+module github.com/th3Hellion
 
 go 1.19
 
 require (
 	github.com/joho/godotenv v1.5.1
-	github.com/rs/cors v1.8.3
+	github.com/rs/cors v1.10.1
 )
--- main.go
+++ main.go
@@ -1,159 +1,175 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "github.com/joho/godotenv"
-  "github.com/rs/cors"
-  "io"
-  "log"
-  "net/http"
-  "os"
-  "time"
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 type VideoData struct {
-  LivestreamStatus string    `json:"livestreamStatus"`
-  VideoID          string    `json:"videoId"`
-  Updated          string    `json:"updated"`
-  FetchedAt        time.Time `json:"fetched_at"`
+	LivestreamStatus string    `json:"livestreamStatus"`
+	VideoID          string    `json:"videoId"`
+	Updated          string    `json:"updated"`
+	FetchedAt        time.Time `json:"fetched_at"`
 }
 
 var videoData VideoData
 var origin = "https://th3Hellion.github.io/Sakamata"
 
-func fetchData() {
-  var channelID = os.Getenv("CHANNEL_ID")
-  var apiKey = os.Getenv("API_KEY")
+func makeRequest(url string, target interface{}) error {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Printf("Failed to fetch data from %s: %v", url, err)
+		return err
+	}
+	defer res.Body.Close()
 
-  url := fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=%s&channelType=any&order=date&type=video&videoCaption=any&videoDefinition=any&videoDimension=any&videoDuration=any&videoEmbeddable=any&videoLicense=any&videoSyndicated=any&videoType=any&key=%s&origin=%s", channelID, apiKey, origin)
-  res, err := http.Get(url)
-  if err != nil {
-    fmt.Println("Failed to fetch the data")
-  }
-  defer res.Body.Close()
+	if err := json.NewDecoder(res.Body).Decode(target); err != nil {
+		log.Printf("Failed to decode JSON response from %s: %v", url, err)
+		return err
+	}
 
-  var result map[string]interface{}
-  if err := json.NewDecoder(res.Body).Decode(&result); err != nil {
-    fmt.Println("Failed to decode the result")
-  }
-
-  items, ok := result["items"].([]interface{})
-  if !ok {
-    itemsInterf := result["items"]
-    if itemsInterf == nil {
-
-      videoData = VideoData{LivestreamStatus: "none", VideoID: "none", Updated: "none", FetchedAt: time.Now()}
-      return
-    }
-    items, ok = itemsInterf.([]interface{})
-    if !ok || len(items) == 0 {
-      videoData = VideoData{LivestreamStatus: "none", VideoID: "none", Updated: "none", FetchedAt: time.Now()}
-      return
-    }
-  }
-
-  var liveItem map[string]interface{}
-  for _, item := range items {
-    itemMap := item.(map[string]interface{})
-    if itemMap["snippet"].(map[string]interface{})["liveBroadcastContent"].(string) == "live" {
-      liveItem = itemMap
-      break
-    }
-  }
-
-  if liveItem != nil {
-    livestreamStatus := liveItem["snippet"].(map[string]interface{})["liveBroadcastContent"].(string)
-    videoID := liveItem["id"].(map[string]interface{})["videoId"].(string)
-    videoData = VideoData{LivestreamStatus: livestreamStatus, VideoID: videoID, Updated: "Stream is Live", FetchedAt: time.Now()}
-  } else {
-    var mostRecentVideo map[string]interface{}
-    for i, item := range items {
-      itemMap := item.(map[string]interface{})
-      publishedAt := itemMap["snippet"].(map[string]interface{})["publishedAt"]
-      if publishedAt == nil || publishedAt == "" {
-        continue
-      }
-      if i == 0 {
-        mostRecentVideo = itemMap
-        continue
-      }
-      if mostRecentVideo["snippet"].(map[string]interface{})["publishedAt"].(string) < publishedAt.(string) {
-        mostRecentVideo = itemMap
-      }
-    }
-    if mostRecentVideo != nil {
-      livestreamStatus := mostRecentVideo["snippet"].(map[string]interface{})["liveBroadcastContent"].(string)
-      videoID := mostRecentVideo["id"].(map[string]interface{})["videoId"].(string)
-      publishedAt := mostRecentVideo["snippet"].(map[string]interface{})["publishedAt"].(string)
-
-      updated := fetchEndTime(videoID, apiKey)
-      if updated == "" {
-        updated = publishedAt // Assign the value of `publishedAt` to `updated`
-      }
-
-      videoData = VideoData{LivestreamStatus: livestreamStatus, VideoID: videoID, Updated: updated, FetchedAt: time.Now()}
-    } else {
-      videoData = VideoData{LivestreamStatus: "none", VideoID: "none", Updated: "none", FetchedAt: time.Now()}
-    }
-
-  }
-
+	return nil
 }
 
-func fetchEndTime(videoId, apiKey string) string {
-  url := fmt.Sprintf("https://youtube.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=%s&key=%s", videoId, apiKey)
-  resp, err := http.Get(url)
-  if err != nil {
-    return ""
-  }
-  defer resp.Body.Close()
-  body, err := io.ReadAll(resp.Body)
-  if err != nil {
-    return ""
-  }
-  var data struct {
-    Items []struct {
-      LiveStreamingDetails struct {
-        ActualEndTime string `json:"actualEndTime"`
-      } `json:"liveStreamingDetails"`
-    } `json:"items"`
-  }
-  err = json.Unmarshal(body, &data)
-  if err != nil {
-    return ""
-  }
-  if len(data.Items) == 0 {
-    return ""
-  }
-  return data.Items[0].LiveStreamingDetails.ActualEndTime
+func handleLiveItem(items []interface{}) {
+	for _, item := range items {
+		itemMap := item.(map[string]interface{})
+		snippet := itemMap["snippet"].(map[string]interface{})
+		if liveBroadcastContent, ok := snippet["liveBroadcastContent"].(string); ok && liveBroadcastContent == "live" {
+			videoData = VideoData{
+				LivestreamStatus: liveBroadcastContent,
+				VideoID:          itemMap["id"].(map[string]interface{})["videoId"].(string),
+				Updated:          "Stream is Live",
+				FetchedAt:        time.Now(),
+			}
+			break
+		}
+	}
+}
+
+func handleMostRecentVideo(items []interface{}) {
+	var mostRecentVideo map[string]interface{}
+
+	for i, item := range items {
+		itemMap := item.(map[string]interface{})
+		publishedAt := itemMap["snippet"].(map[string]interface{})["publishedAt"]
+		if publishedAt == nil || publishedAt == "" {
+			continue
+		}
+
+		if i == 0 || publishedAt.(string) > mostRecentVideo["snippet"].(map[string]interface{})["publishedAt"].(string) {
+			mostRecentVideo = itemMap
+		}
+	}
+
+	if mostRecentVideo != nil {
+		livestreamStatus := mostRecentVideo["snippet"].(map[string]interface{})["liveBroadcastContent"].(string)
+		videoID := mostRecentVideo["id"].(map[string]interface{})["videoId"].(string)
+		publishedAt := mostRecentVideo["snippet"].(map[string]interface{})["publishedAt"].(string)
+
+		updated := fetchEndTime(videoID)
+		if updated == "" {
+			updated = publishedAt
+		}
+
+		videoData = VideoData{
+			LivestreamStatus: livestreamStatus,
+			VideoID:          videoID,
+			Updated:          updated,
+			FetchedAt:        time.Now(),
+		}
+	} else {
+		videoData = VideoData{
+			LivestreamStatus: "none",
+			VideoID:          "none",
+			Updated:          "none",
+			FetchedAt:        time.Now(),
+		}
+	}
+}
+
+func fetchEndTime(videoID string) string {
+	apiKey := os.Getenv("API_KEY")
+	url := fmt.Sprintf("https://youtube.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=%s&key=%s", videoID, apiKey)
+
+	var data struct {
+		Items []struct {
+			LiveStreamingDetails struct {
+				ActualEndTime string `json:"actualEndTime"`
+			} `json:"liveStreamingDetails"`
+		} `json:"items"`
+	}
+
+	if err := makeRequest(url, &data); err != nil {
+		return ""
+	}
+
+	if len(data.Items) == 0 {
+		return ""
+	}
+
+	return data.Items[0].LiveStreamingDetails.ActualEndTime
+}
+
+func fetchData() {
+	godotenv.Load()
+
+	channelID := os.Getenv("CHANNEL_ID")
+	apiKey := os.Getenv("API_KEY")
+
+	url := fmt.Sprintf("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=%s&channelType=any&order=date&type=video&videoCaption=any&videoDefinition=any&videoDimension=any&videoDuration=any&videoEmbeddable=any&videoLicense=any&videoSyndicated=any&videoType=any&key=%s&origin=%s", channelID, apiKey, origin)
+
+	var result map[string]interface{}
+	if err := makeRequest(url, &result); err != nil {
+		return
+	}
+
+	items, ok := result["items"].([]interface{})
+	if !ok || len(items) == 0 {
+		videoData = VideoData{
+			LivestreamStatus: "none",
+			VideoID:          "none",
+			Updated:          "none",
+			FetchedAt:        time.Now(),
+		}
+		return
+	}
+
+	handleLiveItem(items)
+	if videoData.LivestreamStatus == "" {
+		handleMostRecentVideo(items)
+	}
 }
 
 func main() {
-  handler := cors.AllowAll().Handler(http.DefaultServeMux)
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatalf("Error loading .env file")
-  }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
-  fetchData()
+	handler := cors.AllowAll().Handler(http.DefaultServeMux)
 
-  ticker := time.NewTicker(15 * time.Minute)
-  go func() {
-    for range ticker.C {
-      fetchData()
-      fmt.Println("Fetching Data at:", time.Now().Format(time.RFC1123))
-    }
-  }()
+	fetchData()
 
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "GET")
-    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(videoData)
-  })
+	ticker := time.NewTicker(15 * time.Minute)
+	go func() {
+		for range ticker.C {
+			fetchData()
+			fmt.Println("Fetching Data at:", time.Now().Format(time.RFC1123))
+		}
+	}()
 
-  fmt.Println("Listening on port", 3000)
-  log.Fatal(http.ListenAndServe(":3000", handler))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(videoData)
+	})
+
+	fmt.Println("Listening on port", 3000)
+	log.Fatal(http.ListenAndServe(":3000", handler))
 }
